d229dd9b46195a71597d2d1200d639ad0a9e0995
Trinper <frolov24082005@gmail.com>
2023-11-22 22:28:53 +0300

    Added cost calculation

--- NewYearGreetings/src/main/java/com/example/newyeargreetings/Controller.java
+++ NewYearGreetings/src/main/java/com/example/newyeargreetings/Controller.java
@@ -17,15 +17,27 @@ import java.util.Vector;
 
 public class Controller {
 
-    //private boolean start = true;
+
     private Model model = new Model();
-    private ObservableList<String> langs = FXCollections.observableArrayList();
-    private ObservableList<String> langsConcerts = FXCollections.observableArrayList();
-    private ObservableList<String> langsGifts = FXCollections.observableArrayList();
+
+    private final ObservableList<String> langs = FXCollections.observableArrayList();
+    private final ObservableList<String> langsConcerts = FXCollections.observableArrayList();
+    private final ObservableList<String> langsGifts = FXCollections.observableArrayList();
 
     public void setModel(Model model){
         this.model = model;
     }
+    @FXML
+    private CheckBox regularCustomer = new CheckBox();
+
+    @FXML
+    private Label costConcert = new Label();
+
+    @FXML
+    private Label costGift = new Label();
+
+    @FXML
+    private Label cost = new Label();
 
     @FXML
     private ComboBox<String> manufacturersBox = new ComboBox<>();
@@ -36,14 +48,20 @@ public class Controller {
     private ComboBox<String> giftsBox = new ComboBox<>();
 
     private String selectedManufacturer = null;
+    private String selectedConcert = null;
+    private String selectedGift = null;
+    private int costOfSelectedConcert;
+    private int costOfSelectedGift;
 
+    private boolean regular;
     public Controller() throws Exception {
     }
 
     @FXML
     private void initialize() throws Exception {
+
         addManufacturers();
-       manufacturersBox.setOnAction((event) -> {
+        manufacturersBox.setOnAction((event) -> {
            selectedManufacturer = manufacturersBox.getSelectionModel().getSelectedItem();
            try {
                addConcerts();
@@ -52,10 +70,58 @@ public class Controller {
                throw new RuntimeException(e);
            }
        });
+
+        concertsBox.setOnAction((event) ->{
+            selectedConcert = concertsBox.getSelectionModel().getSelectedItem();
+            if (!Objects.equals(selectedConcert, "Select Concert") && selectedConcert != null) {
+                getCostOfConcert();
+                costConcert.setText("Selected concert: " + selectedConcert + '\n' + "Price of concert: " + costOfSelectedConcert);
+                regular = regularCustomer.isSelected();
+                updateCost();
+            }
+        });
+
+        giftsBox.setOnAction((event) ->{
+            selectedGift = giftsBox.getSelectionModel().getSelectedItem();
+            if (!Objects.equals(selectedGift, "Select Gift") && selectedGift != null) {
+                getCostOfGift();
+                costGift.setText("Selected gift: " + selectedGift + '\n' + "Price of gift: " + costOfSelectedGift);
+                regular = regularCustomer.isSelected();
+                updateCost();
+            }
+        });
     }
 
+    public void updateCost(){
+        double finalCost = (regular ? costOfSelectedConcert + costOfSelectedGift * 0.9 : costOfSelectedConcert + costOfSelectedGift);
+        cost.setText("Final cost: " + finalCost);
+    }
+
+    public void getCostOfConcert() {
+        for (var manufacturer : model.manufacturers) {
+            if (Objects.equals(manufacturer.getNameOfManufacturer(), selectedManufacturer)) {
+                for (var concert : manufacturer.getConcerts()) {
+                    if (Objects.equals(concert.getNameOfConcert(),selectedConcert)){
+                        costOfSelectedConcert = concert.getPriceOfConcert();
+                    }
+                }
+            }
+        }
+    }
+
+    public void getCostOfGift() {
+        for (var manufacturer : model.manufacturers) {
+            if (Objects.equals(manufacturer.getNameOfManufacturer(), selectedManufacturer)) {
+                for (var gift : manufacturer.getGifts()) {
+                    if (Objects.equals(gift.getNameOfGift(),selectedGift)){
+                        costOfSelectedGift = gift.getPriceOfGift();
+                    }
+                }
+            }
+        }
+    }
     @FXML
-   public void addManufacturers() throws Exception{
+    public void addManufacturers() throws Exception{
 
         for (Manufacturer manufacturer : model.manufacturers) {
             langs.add(manufacturer.getNameOfManufacturer());
